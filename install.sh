f154f80bd9cfeebb2674c1e9a28c03e5e8192536
ruesei-koseki <51506011+ruesei-koseki@users.noreply.github.com>
2021-12-27 21:48:36 +0900

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo pip3 install pygame
+sudo apt-get install -qq build-essential swig git libpulse-dev libasound2-dev -y
+sudo pip3 install pocketsphinx
+sudo apt-get install ruby-dev g++ make ruby -y
+sudo gem install discordrb
+sudo gem install suika
