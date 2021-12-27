47f60ae54b8d2346d8aa3fce70b01dab6acc5c68
ruesei-koseki <51506011+ruesei-koseki@users.noreply.github.com>
2021-12-27 21:54:29 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+sudo apt intall python3
+sudo apt intall pip
 sudo pip3 install pygame
 sudo apt-get install -qq build-essential swig git libpulse-dev libasound2-dev -y
 sudo pip3 install pocketsphinx
