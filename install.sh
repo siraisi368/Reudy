42065641a458fede3e15a570270e5d4721282f2b
Masato Siraisi <zhaitengkong5@gmail.com>
2022-01-11 16:18:13 +0900

    install.shの修正

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt install -y python3
 sudo apt install -y pip
-sudo apt install portaudio19-dev
+sudo apt install -y portaudio19-dev
 sudo pip3 install mutagen
 sudo pip3 install pygame
 sudo pip3 install bs4
